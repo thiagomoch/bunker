69167c8043eccf80c5ba7239e24d8905ee148f38
Thiago Moch <thiago@moch.com.br>
2020-11-09 12:34:51 -0300

    ajustes

---
+++ bunker-recife/politicas.php
@@ -0,0 +1,89 @@
+<?php 
+define("URL_SITE", 			"http://" . $_SERVER['HTTP_HOST'] . str_replace("index.php", "", $_SERVER['PHP_SELF']));
+
+// include('inc/head.php'); 
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<?php include('inc/head2.php'); ?>
+</head>
+<body id="" class="">
+	
+	<!-- Google Tag Manager -->
+	<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-WG5NKBR"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-WG5NKBR');</script>
+	<!-- End Google Tag Manager -->
+
+	<div class="background">
+		<div id="video-wrap" class="video-wrap">
+			<div class="content-overlay">
+				<div class="d-flex justify-content-center pt-3 mb-5">
+					<h1 class="header-logo">
+						<a href="http://www.bunkercf.com.br">
+							<img src="img/marca.png" height="30" alt="Bunker Crossfit">
+							<span class="sr-only">Bunker Crossfit</span>
+						</a>
+					</h1>
+				</div>
+				<section class="container highlight text-center">
+					<h2>Políticas de Privacidade</h2>
+					<blockquote>
+				  <p>A privacidade dos nossos visitantes é muito importante.</p>
+				</blockquote>
+				<p>No nosso site, reconhecemos a importância da privacidade e tudo faremos para a respeitar. Em  seguida apresento-lhe o tipo de informação pessoal que recebemos e coletamos quando você visita este site , e bem como guardamos essa informação.</p>
+
+				<p>Nunca venderemos a sua informação para terceiros, sejam eles particulares ou empresas.</p>
+
+				<p><strong>Os registos:</strong></p>
+
+				Tal como outros websites, coletamos e utilizamos informação contida nos registos. A informação contida nos registos inclui, o seu ISP (Internet Service Provider), o seu endereço IP (Internet Protocol), o browser que utilizou ao visitar o nosso website (ex: Firefox), o tempo da sua visita e que páginas visitou dentro do nosso website.<p></p>
+
+				<p><strong>Os Cookies e Web Beacons:</strong></p>
+
+				<p>Utilizamos cookies para armazenar informação, tais como as suas preferências pessoais quando visita o nosso website. Isto poderá incluir um simples pop-up, ou mesmo uma ligação em vários serviços que providenciamos.</p>
+
+				<p>A Google faz uso do novo DART cookie para servir anúncios baseados nos seus interesses e na sua visita ao nosso site e a outros endereços na web. Poderá optar por não utilizar o DART cookie visitando a página de anúncios do Google e o “programa de policiamento” da empresa.</p>
+
+				<p>Em adição, no nosso site  também utilizamos publicidade de terceiros. Alguns destes anúncios (sponsors), poderão utilizar tecnologias como os cookies e/ou web beacons quando anunciam  no nosso website, o que fará com que esses publicitários (tal como o Google através do Google AdSense) também recebam a sua informação pessoal, como o seu ISP , o endereço IP, o seu browser, e outros.</p>
+				</section>
+				<!-- <div class="container">
+					<header>
+						<div class="container">
+							<div class="brand text-center">
+								<h1 class="header-logo">
+									<a href="http://www.bunkercf.com.br">
+										<img src="img/marca.png" height="30" alt="Bunker Crossfit">
+										<span class="sr-only">Bunker Crossfit</span>
+									</a>
+								</h1>
+							</div>
+						</div>
+					</header>
+					<section class="highlight highlight-obrigado text-center">
+						<h2>Agora vai!</h2>
+						<p>Estamos muito felizes que você deu o primeiro passo para a mudança, em breve entraremos em contato para marcar o melhorar horário da sua aula experimental.</p>
+						<div class="container">
+							<div class="row">
+								<div class="col-lg-6 col-lg-offset-3">
+									<div class="embed-responsive embed-responsive-16by9">
+									  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/Iq_BmYcCQtw?rel=0&amp;showinfo=0&amp;autoplay=1&amp;controls=0"></iframe>
+									</div>
+								</div>
+								
+							</div>
+						</div>
+					</section>
+		    	</div> -->
+			</div>
+		</div>
+	</div>
+
+	<?php include('inc/footer.php'); ?>
+</body>
+</html>
